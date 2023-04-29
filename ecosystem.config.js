96f92254c9f741641d5d119245eee154d7f5863c
anderCM <andercm15@gmail.com>
2023-04-29 18:05:44 -0500

    Project config renamed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,16 +4,19 @@ module.exports = {
       name: 'upfic',
       script: 'yarn',
       args: 'start',
-      cwd: '/var/www/aula.upfic.pe/upfic'
+      cwd: '/var/www/aula.upfic.pe/react',
+      env: {
+        PORT: 3000
+      }
     }],
     deploy: {
       production: {
         user: 'ubuntu',
         host: 'localhost',
         ref: 'origin/main',
-        repo: 'git@github.com:anderCM/react-app.git',
+        repo: 'git@github.com:anderCM/react.git',
         branch: 'main',
-        path: '/var/www/aula.upfic.pe/upfic',
+        path: '/var/www/aula.upfic.pe/react',
         ssh_options: ['StrictHostKeyChecking=no', 'PasswordAuthentication=no', 'IdentityFile=~/.ssh/ssh_andercm_github', 'UserKnownHostsFile=/dev/null', 'IdentitiesOnly=yes', 'AddKeysToAgent=yes', 'ForwardAgent=yes', 'PreferredAuthentications=publickey', 'PasswordAuthentication=no', 'User=anderCM', 'IdentityFile=~/.ssh/ssh_andercm_github', 'AddKeysToAgent=yes'],
         'post-deploy': 'yarn install && pm2 reload ecosystem.config.js --env production'
       }
