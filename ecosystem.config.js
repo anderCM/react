045f977dadcecd2bc547e0ada90f9748a6e4bee4
anderCM <andercm15@gmail.com>
2023-04-29 18:39:36 -0500

    SSH key file changed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
         repo: 'git@github.com:anderCM/react.git',
         branch: 'main',
         path: '/var/www/aula.upfic.pe/react',
-        ssh_options: ['StrictHostKeyChecking=no', 'PasswordAuthentication=no', 'IdentityFile=~/.ssh/ssh_andercm_github', 'UserKnownHostsFile=/dev/null', 'IdentitiesOnly=yes', 'AddKeysToAgent=yes', 'ForwardAgent=yes', 'PreferredAuthentications=publickey', 'PasswordAuthentication=no', 'User=anderCM', 'IdentityFile=~/.ssh/ssh_andercm_github', 'AddKeysToAgent=yes'],
+        ssh_options: ['StrictHostKeyChecking=no', 'PasswordAuthentication=no', 'IdentityFile=~/.ssh/id_rsa', 'UserKnownHostsFile=/dev/null', 'IdentitiesOnly=yes', 'AddKeysToAgent=yes', 'ForwardAgent=yes', 'PreferredAuthentications=publickey', 'PasswordAuthentication=no', 'User=anderCM', 'IdentityFile=~/.ssh/id_rsa', 'AddKeysToAgent=yes'],
         'post-deploy': 'yarn install && pm2 reload ecosystem.config.js --env production'
       }
     }
